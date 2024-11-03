3e04659e31afcd8de8ce25d3677f0f30198f3c41
Sai Govardhan Naidu Gavireddy <sgavired@gitam.in>
2024-11-03 11:38:08 +0530

    Displaying Jobs

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bangalore, India',
+        'Salary': 'Rs. 1000000',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Delhi, India',
+        'Salary': 'Rs. 1200000',
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Developer',
+        'location':'Remote',
+        # 'Salary': '800000',
+    },
+    {
+        'id': 4,
+        'title': 'Backend Developer',
+        'location':'Remote',
+        'Salary': 'Rs. 900000',
+    }
+]
+
 @app.route('/')
-def hello_world():
-    return render_template('home.html')
+def home():
+    return render_template('home.html',jobs = JOBS)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
